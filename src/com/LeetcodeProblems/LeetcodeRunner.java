a6fc1b91064088cb16ed6a1cd0ba6c5b43388028
Divya Kapoor <divya@Divyas-MacBook-Pro.local>
2024-08-08 14:19:33 -0700

    updated with leftandrightsumdifference

--- src/com/LeetcodeProblems/LeetcodeRunner.java
+++ src/com/LeetcodeProblems/LeetcodeRunner.java
@@ -4,9 +4,15 @@ public class LeetcodeRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// number of senior citizens
 		String [] details = {"7868190130M7522","5303914400F9211","9273338290F4010"};
 		NumberOfSeniorCitizens x = new NumberOfSeniorCitizens(details);
 		x.countSeniors(details);
+		
+		//left and right sum difference
+		int[] nums = {10, 4, 8, 3};
+		LeftAndRightSumDifference dif = new LeftAndRightSumDifference(nums);
+		dif.leftRightDifference(nums);
 	}
 
 }
