68f3b30ec08a02217105d5c07cfacd22f6956f31
Divya Kapoor <divya@Divyas-MacBook-Pro.local>
2024-08-07 23:55:31 -0700

    first commit leetcodeRunner

---
+++ src/com/LeetcodeProblems/LeetcodeRunner.java
@@ -0,0 +1,12 @@
+package com.LeetcodeProblems;
+
+public class LeetcodeRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] details = {"7868190130M7522","5303914400F9211","9273338290F4010"};
+		NumberOfSeniorCitizens x = new NumberOfSeniorCitizens(details);
+		x.countSeniors(details);
+	}
+
+}
