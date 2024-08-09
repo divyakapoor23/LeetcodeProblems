8f39734454e947cac9b8ac4138a2b257dcb8e3e0
Divya Kapoor <divya@Divyas-MacBook-Pro.local>
2024-08-08 17:29:02 -0700

    MakeTwoArraysEqualByReversingSubarrays Added

--- src/com/LeetcodeProblems/LeetcodeRunner.java
+++ src/com/LeetcodeProblems/LeetcodeRunner.java
@@ -7,12 +7,20 @@ public class LeetcodeRunner {
 		// number of senior citizens
 		String [] details = {"7868190130M7522","5303914400F9211","9273338290F4010"};
 		NumberOfSeniorCitizens x = new NumberOfSeniorCitizens(details);
-		x.countSeniors(details);
+//		x.countSeniors(details);
 		
 		//left and right sum difference
 		int[] nums = {10, 4, 8, 3};
 		LeftAndRightSumDifference dif = new LeftAndRightSumDifference(nums);
-		dif.leftRightDifference(nums);
-	}
+//		dif.leftRightDifference(nums);
+		
+		
+		int[] target = {1,2,3,4};
+		int[] arr = {2,4,2,1};
+		MakeTwoArraysEqualByReversingSubarrays eq = new MakeTwoArraysEqualByReversingSubarrays(target, arr);
+		eq.canBeEqual(target, arr);
 
+	}
+	
+	
 }
